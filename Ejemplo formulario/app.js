8af9f82cfe1425d7f5d3ead841feacc88c2b04bd
julianisaac <julianisaac@gmail.com>
2022-03-16 11:26:02 -0300

    subo creacion de proyecto del entregable de clase 2

--- Ejemplo formulario/app.js
+++
@@ -1,5 +0,0 @@
-formulario.addEventListener('submit', (e)=> {
-    e.preventDefault();
-    let datos = `Nombre: ${fname.value} - Apellido: ${lname.value} - Descripcion: ${task.value}`
-    console.log(datos);
-})
