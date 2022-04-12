8072b8b770fed8e77f90ddec296b003c88f09e40
julianisaac <julianisaac@gmail.com>
2022-04-12 20:26:33 -0300

    entrega firebase I parcial

--- ecommerce-julianisaac/src/components/ItemList/index.jsx
+++ ecommerce-julianisaac/src/components/ItemList/index.jsx
@@ -1,6 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import Item from '../Item';
+import { db } from '../../firebase/config';
+import { collection, getDocs } from "firebase/firestore";
 
 const ItemList = ({categoriaId}) => {
 
@@ -11,14 +13,24 @@ const ItemList = ({categoriaId}) => {
     console.log(`categoriaId: ${categoriaId}`);
     ( async ()=> {
         try {
+          /*
             const response = await fetch(`https://fakestoreapi.com/products`)
             const data = await response.json();
+*/
+            const querySnapshot = await getDocs(collection(db, "productos"));
+            const prods = []
+            querySnapshot.forEach((doc) => {
+                // doc.data() is never undefined for query doc snapshots
+                prods.push({id: doc.id, ...doc.data()})
+            });  
+            console.log(prods);        
+
             if (categoriaId) {
-              const productosFiltrados = data.filter(prod => prod.category === categoriaId)
+              const productosFiltrados = prods.filter(prod => prod.category === categoriaId)
               setProductos(productosFiltrados);
             }
             else{
-              setProductos(data);
+              setProductos(prods);
             }
             
         } catch (error) {
