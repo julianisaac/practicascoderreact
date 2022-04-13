f62de59acbc2378b697388a7c41f929b0c59ee7d
julianisaac <julianisaac@gmail.com>
2022-04-12 22:08:34 -0300

    subo entrega firebase I

--- ecommerce-julianisaac/src/components/ItemList/index.jsx
+++ ecommerce-julianisaac/src/components/ItemList/index.jsx
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import Item from '../Item';
 import { db } from '../../firebase/config';
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, query, where } from "firebase/firestore";
 
 const ItemList = ({categoriaId}) => {
 
@@ -17,19 +17,24 @@ const ItemList = ({categoriaId}) => {
             const response = await fetch(`https://fakestoreapi.com/products`)
             const data = await response.json();
 */
-            const querySnapshot = await getDocs(collection(db, "productos"));
-            const prods = []
-            querySnapshot.forEach((doc) => {
-                // doc.data() is never undefined for query doc snapshots
-                prods.push({id: doc.id, ...doc.data()})
-            });  
-            console.log(prods);        
-
+            const items = collection(db, "productos");
             if (categoriaId) {
-              const productosFiltrados = prods.filter(prod => prod.category === categoriaId)
-              setProductos(productosFiltrados);
+              const q = query(items, where("category", "==", categoriaId));
+              getDocs(q).then((snapshot) => {
+                setProductos(snapshot.docs.map((doc)=>({id: doc.id, ...doc.data()})))  
+              })
+              /*
+                const productosFiltrados = prods.filter(prod => prod.category === categoriaId)
+                setProductos(productosFiltrados);
+              */
             }
             else{
+              const querySnapshot = await getDocs(items);
+              const prods = []
+              querySnapshot.forEach((doc) => {
+                  // doc.data() is never undefined for query doc snapshots
+                  prods.push({id: doc.id, ...doc.data()})
+              });    
               setProductos(prods);
             }
             
--- ecommerce-julianisaac/src/containers/ItemDetailContainer/index.jsx
+++ ecommerce-julianisaac/src/containers/ItemDetailContainer/index.jsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import ItemDetail from '../../components/ItemDetail';
 
+import { db } from '../../firebase/config';
+import { doc, getDoc } from "firebase/firestore";
+
 const ItemDetailContainer = () => {
 
     const [producto, setProducto] = useState({});
@@ -11,9 +14,21 @@ const ItemDetailContainer = () => {
     useEffect(()=> {
         ( async ()=> {
             try {
+                /*
                 const response = await fetch(`https://fakestoreapi.com/products/${id}`)
                 const data = await response.json();
                 setProducto(data);
+                */
+                const docRef = doc(db, "productos", id);
+                const docSnap = await getDoc(docRef);
+
+                if (docSnap.exists()) {
+                    setProducto({id: docSnap.id, ...docSnap.data()})
+                } else {
+                    // doc.data() will be undefined in this case
+                    console.log("No such document!");
+                }
+
             } catch (error) {
                 console.log(error);
             }
